b6916b8e755df570bedd134e6752f266ed7e4c2b
Steve Schmechel <steves@msidata.com>
2018-01-08 10:40:53 -0600

    Update company version

--- index.js
+++ index.js
@@ -67,7 +67,7 @@ request({
                                 'APIKey': c.Settings.APIKey,
                                 'HostUrl': c.Settings.HostUrl,
                                 'Enabled': enabled,
-                                'CompanyVer': c.CompanyVersionNumber,
+                                'CompanyVer': company.CompanyVersionNumber,
                                 'APIVer': resSpie ? resSpie.IntegrationEngineVersion : enabled ? defVer : 'n/a'
                             };
                             output.push(entry);
